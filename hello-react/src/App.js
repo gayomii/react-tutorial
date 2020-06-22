11698a08f8b08284793aee7b9257b99482beae5c
Gayeon Lee <rkdus1006@gmail.com>
2020-06-22 17:50:05 +0900

    eventHandling, component lifeCycle

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import Hello from './Hello';
-import './App.css';
-import Wrapper from './Wrapper';
-import Counter from './Counter';
+import React, { Component } from 'react';
+import LifeCycleSample from './LifeCycleSample';
 
-function App() {
-  return (
-    <Wrapper>
-      <div className='div-box'>Hello React</div>
-      <br></br>
-      <Hello name='react' color='blue' isSpecial='true' />
-      <Hello color='skyblue' />
-      <Counter />
-    </Wrapper>
-  );
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -9,11 +9,17 @@ function Counter(){
     const onClickLeave = () => {
         setMessage('안녕히가세요');
     }
+    const [color, setColor] = useState('black');
+
     return (
         <div>
-            <h1>{message}</h1>
+            <br />
             <button onClick={onClickEnter}>입장</button>
             <button onClick={onClickLeave}>퇴장</button>
+            <h1 style={{ color }}>{message}</h1>
+            <button style={{ color: 'red'}} onClick={()=>setColor('red')}>빨강</button>
+            <button style={{ color: 'green'}} onClick={()=>setColor('green')}>초록</button>
+            <button style={{ color: 'blue'}} onClick={()=>setColor('blue')}>파랑</button>
         </div>
     );
 }
